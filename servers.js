c67ac49b7aa6ef79ddac03a313e853d890124b89
syphrose <swabulwenyi@gmail.com>
2024-03-13 23:49:49 +0300

    The server

--- servers.js
+++ servers.js
@@ -1,15 +1,12 @@
 import express from 'express';
 import router from './routes/index';
 
-const port = parseInt(process.env.PORT, 10) || 5000;
-
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use('/', router);
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
-
-export default app;
